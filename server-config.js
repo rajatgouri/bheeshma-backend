a3b62370af0ea6d7ba05d1275f5079a9543a76d3
rajatgouri <rajatgouri020@gmail.com>
2020-09-12 18:44:48 +0530

    add database

--- server-config.js
+++ server-config.js
@@ -1,7 +1,7 @@
 module.exports = {
     jwt_secret_key : "123456",
     jwt_ExpiresIn: '40m',
-    mongoURI : "mongodb+srv://Rajat:Rajat@123@cluster0.lgak6.mongodb.net/Bheeshma?retryWrites=true&w=majority",
+    mongoURI : "mongodb+srv://Rajat:Rajat@123@cluster0.h87jl.gcp.mongodb.net/Bheeshma?retryWrites=true&w=majority",
     gmail: "rajatgouri020@gmail.com",
     gmailPassword: "sunitarani",
     resetTokenExpiration : 5 * 60 * 1000
